d68967b5741e62c9ac12023d957879ff4632c85b
Alexey <52752908+volkovAlexey@users.noreply.github.com>
2020-12-13 20:15:15 +0200

    add method numJewelsInStones

---
+++ src/com/my_company/task4/Jewel.java
@@ -0,0 +1,28 @@
+package com.my_company.task4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Jewel {
+
+    public int numJewelsInStones(String jewels, String stones) {
+        int numJewels = 0;
+        String regex = "[A-Za_z]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcherJewels = pattern.matcher(jewels);
+        Matcher matcherStones = pattern.matcher(stones);
+        if (matcherJewels.find() && matcherStones.find()) {
+            char[] charsJewels = jewels.toCharArray();
+            char[] charsStones = stones.toCharArray();
+            for (char charJewel : charsJewels) {
+                for (char charStone : charsStones) {
+                    if (charJewel == charStone) {
+                        numJewels++;
+                    }
+                }
+
+            }
+        }
+        return numJewels;
+    }
+}
---
+++ src/com/my_company/task4/Main.java
@@ -0,0 +1,9 @@
+package com.my_company.task4;
+
+public class Main {
+    public static void main(String[] args) {
+        Jewel jewel = new Jewel();
+        int a = jewel.numJewelsInStones("qwerty", "AwdFWwe");
+        System.out.println(a);
+    }
+}
