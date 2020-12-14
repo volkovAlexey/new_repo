79c71223fc8590b3c93fcc105280aa0661a355f1
Alexey <52752908+volkovAlexey@users.noreply.github.com>
2020-12-14 09:41:06 +0200

    add class SortArray and method bubbleSort

---
+++ src/com/my_company/task4/SortArray.java
@@ -0,0 +1,17 @@
+package com.my_company.task4;
+
+public class SortArray {
+
+    public void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+}
