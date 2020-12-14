6421190f16825c8f0966cb4bbac813ca37c48bc7
Alexey <52752908+volkovAlexey@users.noreply.github.com>
2020-12-14 13:09:35 +0200

    refactor method sum

--- src/com/my_company/task5/Calculator.java
+++ src/com/my_company/task5/Calculator.java
@@ -2,7 +2,11 @@ package com.my_company.task5;
 
 public class Calculator {
 
-    public int sum(int a, int b) {
-        return a + b;
+    public int sum(int[] arr) {
+        int sum = 0;
+        for (int temp : arr) {
+            sum = sum + temp;
+        }
+        return sum;
     }
 }
