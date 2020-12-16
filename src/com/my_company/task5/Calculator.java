0858bd34d64e1992bf96a1a2b864e2a329abb8ca
Alexey <52752908+volkovAlexey@users.noreply.github.com>
2020-12-16 10:27:38 +0200

    change plus

--- src/com/my_company/task5/Calculator.java
+++ src/com/my_company/task5/Calculator.java
@@ -2,7 +2,11 @@ package com.my_company.task5;
 
 public class Calculator {
 
-    public int plus(int a, int b) {
-        return a + b;
+    public int plus(int[] arr) {
+        int sum = 0;
+        for (int temp : arr) {
+            sum += temp;
+        }
+        return sum;
     }
 }
