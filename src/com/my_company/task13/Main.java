53f0b6f30099709544ee1bccf930b0a13b5a6f19
Alexey <52752908+volkovAlexey@users.noreply.github.com>
2021-01-23 10:27:42 +0200

    calculate time in lecture

--- src/com/my_company/task13/Main.java
+++ src/com/my_company/task13/Main.java
@@ -1,10 +1,8 @@
 package com.my_company.task13;
-//todo С помощью класса LocalDateTime посчитать сколько дней,
-// часов, минут и секунд проходит между началом лекции в
-// понедельник и концом лекции в четверг.
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +10,9 @@ public class Main {
         LocalDateTime toDate = LocalDateTime.of(2021, 01, 21, 22, 00, 00);
         System.out.println("Passed between lectures - " + getPeriod(fromDate, toDate));
 
+        LocalTime start = LocalTime.of(20, 0, 0);
+        LocalTime finish = LocalTime.of(22, 0, 0);
+        System.out.println("Time in lecture - " + getTime(start, finish));
     }
 
     private static String getPeriod(LocalDateTime fromDate, LocalDateTime toDate) {
@@ -20,8 +21,11 @@ public class Main {
         long seconds = duration.getSeconds();
         long minutes = duration.toMinutes();
         long hours = duration.toHours();
-        return "days: " + days + " seconds: " + seconds + " minutes: " + minutes + " hours: " + hours;
+        return "days: " + days + " hours: " + hours + " minutes: " + minutes + " seconds: " + seconds;
     }
 
-
+    private static String getTime(LocalTime start, LocalTime finish) {
+        Duration duration = Duration.between(start, finish);
+        return "minutes: " + duration.toMinutes() + " seconds: " + duration.getSeconds();
+    }
 }
