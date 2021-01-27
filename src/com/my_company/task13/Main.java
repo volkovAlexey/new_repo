61e649e26abcc3e4f6290cace67df99dbdab98f5
Alexey <52752908+volkovAlexey@users.noreply.github.com>
2021-01-27 14:34:18 +0200

    fixed code

--- src/com/my_company/task13/Main.java
+++ src/com/my_company/task13/Main.java
@@ -21,8 +21,8 @@ public class Main {
 
         LocalDateTime date = LocalDateTime.parse("1993/Jun/22 07:34:56", formatter);
         System.out.println(date);
-        LocalDateTime date1 = LocalDateTime.parse("22:10 1993/12/25", formatter1);
-        System.out.println(date1);
+
+        System.out.println(LocalDateTime.now().format(formatter1));
 
         Long minutes = time.calculateMinutesInSeason("Summer");
         System.out.println(minutes);
